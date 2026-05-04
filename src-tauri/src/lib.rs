514379693ccfd0f9d8cdda6a49ac51025a7eb907
Kirk Lin <linkirk@163.com>
2026-05-04 22:36:42 +0800

    fix: update autostart launcher from LaunchAgent to AppleScript for macOS compatibility

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -20,7 +20,7 @@ pub fn run() {
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_autostart::init(
-            tauri_plugin_autostart::MacosLauncher::LaunchAgent,
+            tauri_plugin_autostart::MacosLauncher::AppleScript,
             None,
         ))
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
